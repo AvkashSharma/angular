18048fec4d30eac42a87339936f731f7fec0f687
Avkash <avkaashsharma89@gmail.com>
2020-05-10 15:05:50 -0400

    Project Init learning module system

---
+++ comps/src/app/collections/collections.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollectionsRoutingModule } from './collections-routing.module';
+import { ComponentHomeComponent } from './component-home/component-home.component';
+
+@NgModule({
+	declarations: [ ComponentHomeComponent ],
+	imports: [ CommonModule, CollectionsRoutingModule ],
+	exports: [ ComponentHomeComponent ]
+})
+export class CollectionsModule {}
